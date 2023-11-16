8d3f76e0b414f999817db8d52fa2beca096d8df7
김산해 <sanhae1996@gmail.com>
2023-11-16 20:15:45 +0900

    Created React Component

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,12 +1,51 @@
 //import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      My Todo Application
+      My Todo application
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <FourthComponent></FourthComponent>
     </div>
+
   );
 }
+
+function FirstComponent()
+{
+  return (
+    <div className="FirstComponent">First Component</div>
+  );
+}
+
+function SecondComponent()
+{
+  return (
+    <div className="SecondComponent">Second Component</div>
+  )
+}
+
+class ThirdComponent extends Component
+{
+  render() {
+    return (
+      <div className="ThirdComponent">Third Component</div>
+    )
+  }
+}
+
+class FourthComponent extends Component
+{
+  render() {
+    return (
+      <div className="FourthComponent">Fourth Component</div>
+    )
+  }
+}
+
 
 export default App;
