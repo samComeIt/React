c54e99df2299f4d00b1ebf4fe7175dcefe9e3507
김산해 <sanhae1996@gmail.com>
2023-11-18 20:08:09 +0900

    Refactoring to Modules

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,54 +1,11 @@
 //import logo from './logo.svg';
-import { Component } from 'react';
 import './App.css';
+import LearningComponent from './components/learning-examples/LearningComponent';
 
 function App() {
   return (
-    <div className="App">
-      My Todo application
-      <FirstComponent />
-      <SecondComponent />
-      <ThirdComponent />
-      <FourthComponent />
-    </div>
-
+    <LearningComponent />
   );
 }
-
-function FirstComponent()
-{
-  return (
-    <div className="FirstComponent">First Component</div>
-  );
-}
-
-function SecondComponent()
-{
-  return (
-    <div className="SecondComponent">Second Component</div>
-  )
-}
-
-class ThirdComponent extends Component
-{
-  render() {
-    return (
-      <div className="ThirdComponent">Third Component</div>
-    )
-  }
-}
-
-class FourthComponent extends Component
-{
-  render() {
-    return (
-      <div>
-      <div className="FourthComponent">Fourth Component</div>
-      <div className="FourthComponent">Fourth Component</div>
-      </div>
-    )
-  }
-}
-
 
 export default App;
---
+++ todo-app/src/components/learning-examples/LearningComponent.jsx
@@ -0,0 +1,18 @@
+import FirstComponent from './FirstComponent';
+import {FifthComponent} from './FirstComponent';
+import SecondComponent from './SecondComponent';
+import ThirdComponent from './ThirdComponent';
+import FourthComponent from './FourthComponent';
+
+export default function LearningComponent() {
+    return (<div className="App">
+      My Todo application
+      <FirstComponent />
+      <SecondComponent />
+      <ThirdComponent />
+      <FourthComponent />
+      <FifthComponent />
+    </div>
+    )
+}
+
