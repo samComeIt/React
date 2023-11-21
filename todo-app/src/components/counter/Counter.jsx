e547b12d07e9156b0677f7511056f48905c60559
김산해 <sanhae1996@gmail.com>
2023-11-22 08:21:00 +0900

    Add decrement function

--- todo-app/src/components/counter/Counter.jsx
+++ todo-app/src/components/counter/Counter.jsx
@@ -10,6 +10,10 @@ export default function Counter() {
         setCount(count + 1)
     }
 
+    function decrementCounterFunction() {
+        setCount(count - 1)
+    }
+
     return (
         <div className="Counter">
             <span className="count">{count}</span>
@@ -17,7 +21,16 @@ export default function Counter() {
                 <button className="counterButton" 
                     onClick={incrementCounterFunction}
                 >+1</button>
+                <button className="counterButton"
+                    onClick={decrementCounterFunction}>-1</button>
             </div>
         </div>
     )
+
+    // We updated the state => React updated the view
+    // A HTML page is represented by DOM
+    // Each element in a HTML page is a node in the DOM
+    // You need tp update the DOM to update the element
+    // However, writing code to update the DOM can be complex and slow
+    
 }
