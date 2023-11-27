8d7b30238107be10027218c07adcfb169e120071
김산해 <sanhae1996@gmail.com>
2023-11-27 09:22:17 +0900

    Improving Login Component Further - Todo React App

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+import './TodoApp.css';
+
 export default function TodoApp()
 {
     return (
@@ -10,16 +13,31 @@ export default function TodoApp()
 }
 
 function LoginComponent() {
+
+    const [username, setUsername] = useState('sammy')
+
+    const [password, setPassword] = useState('')
+
+    function handleUsernameChange(event) {
+        // console.log(event);
+        // console.log(event.target.value);
+        setUsername(event.target.value)
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value)
+    }
+
     return (
         <div className="Login">
             <div className="LoginForm">
                 <div>
-                    <label>User Name</label>
-                    <input type="text" name="username" />
+                    <label>User Name:</label>
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div>
-                    <label>Password</label>
-                    <input type="passwrod" name="passwrod" />
+                    <label>Password:</label>
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
                     <button type="button" name="Login" >Login</button>
