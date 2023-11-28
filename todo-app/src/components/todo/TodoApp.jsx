3db76862ade53313b9130996563645895e425c0d
김산해 <sanhae1996@gmail.com>
2023-11-29 08:06:12 +0900

    Adding Bootstrap to React Front End Application

--- todo-app/src/components/todo/TodoApp.jsx
+++ todo-app/src/components/todo/TodoApp.jsx
@@ -170,10 +170,10 @@ function ListTodosComponent() {
 
 
     return (
-        <div className="ListTodosComponent">
+        <div className="container">
             <h1>Things you want to do!</h1>
             <div>
-               <table>
+               <table className="table">
                    <thead>
                         <tr>
                             <td>id</td>
