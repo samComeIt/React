49fdd72f60402b5f435b8bf322a9902248333391
김산해 <sanhae1996@gmail.com>
2023-11-30 18:04:24 +0900

    Updating React State and Verifying Updates through Auth Context

--- todo-app/src/components/todo/HeaderComponent.jsx
+++ todo-app/src/components/todo/HeaderComponent.jsx
@@ -1,12 +1,11 @@
-import { useContext } from 'react'
 import {Link} from 'react-router-dom'
-import {AuthContext} from './security/AuthContext'
+import {useAuth} from './security/AuthContext'
 
 function HeaderComponent() {
 
-    const authContext = useContext(AuthContext)
-    console.log(authContext.value);
-    
+    //const authContext = useContext(AuthContext)
+    //console.log(authContext.value);
+    const authContext = useAuth()
 
     return (
         <header className="border-bottom border-light border-5 mb-5 p-2">
