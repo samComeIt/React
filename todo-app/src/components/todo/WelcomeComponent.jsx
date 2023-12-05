e7f942ce8a20978507dc4bc40b60d8cd6737ce2d
김산해 <sanhae1996@gmail.com>
2023-12-05 21:46:01 +0900

    Invoking Spring Boot Hello World Bean & Path Param REST API from React

--- todo-app/src/components/todo/WelcomeComponent.jsx
+++ todo-app/src/components/todo/WelcomeComponent.jsx
@@ -1,11 +1,19 @@
 import {useParams, Link} from 'react-router-dom'
 import axios from 'axios'
+import { useState } from 'react'
 function WelcomeComponent() {
     const {username} = useParams()
 
+    const [message, setMessage] = useState(null)
+
     function callHelloWorldRestApi()
     {
-        axios.get('http://localhost:8080/hello-world')
+        // axios.get('http://localhost:8080/hello-world')
+        // .then((response) => successfulResponse(response))
+        // .catch((error) => errorResponse(error))
+        // .finally(() => console.log('cleanup'))
+
+        axios.get('http://localhost:8080/hello-world-bean')
         .then((response) => successfulResponse(response))
         .catch((error) => errorResponse(error))
         .finally(() => console.log('cleanup'))
@@ -14,6 +22,7 @@ function WelcomeComponent() {
     function successfulResponse(response)
     {
         console.log(response)
+        setMessage(response.data.message)
     }
 
     function errorResponse(error)
@@ -30,6 +39,7 @@ function WelcomeComponent() {
         <div>
             <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World</button>
         </div>
+        <div className="text-info">{message}</div>
         </div>
     )
 }
